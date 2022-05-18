04c5433ee92bac352b7184732ef18562b3a0bd50
rosyoon <yjlim9898@gmail.com>
2022-05-18 10:20:32 +0900

    Update recoil.js

--- src/store/recoil.js
+++ src/store/recoil.js
@@ -1,5 +1,5 @@
 import { atom } from "recoil";
 export const page = atom({
-  key: "page", // unique ID (with respect to other atoms/selectors)
-  default: 1, // default value (aka initial value)
+  key: "page", 
+  default: 1, 
 });
